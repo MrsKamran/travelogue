10c7255ed24d3bcc766b3c60691eec56c44f593b
Terence Tan <terencetan@Terences-MacBook-Pro.local>
2020-11-14 10:07:31 -0500

    Added login/logout page and templates. Changed base.html so that the links work and added an if statement to show login/logout for logged in users. Added crispy to apps so we can use it for forms

--- users/views.py
+++ users/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
+from django.contrib.messages import constants as messages
+
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/register/')
+            # message.success(request, f'Account has been created, you are now able to login!')
+            return redirect('login')
     else: 
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
 
