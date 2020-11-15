aa203e4267323a518fe3260305b2526ff8529aa4
Nivetha Nagaratnam <Nivetha@MacBook-Air-2.local>
2020-11-14 19:02:40 -0500

    adding the details page code

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,9 @@ from .views import PostCreate
 urlpatterns = [
   path('', views.home, name='home'),
   path('index/', views.index, name="index" ),
+  path('index/<int:posts_id>/', views.posts_detail, name='detail'),
   path('index/create/', views.PostCreate.as_view(), name='posts_create'),
+  path('index/<int:posts_id>/add_review/', views.add_review, name='add_review'),
+  path('index/<int:posts_id>/add_photo/', views.add_photo, name='add_photo'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,36 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import uuid
 import boto3
 from .models import Posts, Photo
+from .forms import ReviewsForm
 from django.views.generic.edit import CreateView
 
 
-
-
 #This is the Amazon S3 Add A Photo View
-S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
-BUCKET = 'travelogue1'
-
-def add_photo(request, cat_id):
-    # photo-file will be the "name" attribute on the <input type="file">
-    photo_file = request.FILES.get('photo-file', None)
-    if photo_file:
-        session = boto3.Session(profile_name='dev')
-        dev_s3_client = session.client('s3')
-        # s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
-        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
-        try:
-            s3.upload_fileobj(photo_file, BUCKET, key)
-            # build the full url string
-            url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
-            photo = Photo(url=url, posts_id=posts_id)
-            photo.save()
-        except:
-            print('An error occurred uploading file to S3')
-    return redirect('detail', posts_id=posts_id)
+S3_BASE_URL = 'https://s3.ca-central-1.amazonaws.com/'
+BUCKET = 'travelogue2'
 
 def home(request):
     #This code gets the last 3 posts in the list
@@ -49,3 +27,40 @@ def index(request):
     #This code gets the last 3 posts in the list
     posts = Posts.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+def posts_detail(request, posts_id):
+    posts = Posts.objects.get(id=posts_id)
+    reviews_form = ReviewsForm()
+    return render(request, 'detail.html', { 'posts': posts, 'reviews_form': reviews_form })
+
+def add_review(request, posts_id):
+     # create a ModelForm instance using the data in request.POST
+    form = ReviewsForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the post_id assigned
+        new_review = form.save(commit=False)
+        new_review.posts_id = posts_id
+        new_review.save()
+    return redirect('detail', posts_id=posts_id)
+
+def add_photo(request, posts_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = Photo(url=url, posts_id=posts_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', posts_id=posts_id)
+    
