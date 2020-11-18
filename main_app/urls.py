21b27ace0817dfdb8521005faf6003b1bcbb9ff1
MrsKamran <67755998+MrsKamran@users.noreply.github.com>
2020-11-18 17:18:53 -0500

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
   path('search/', SearchResultsView.as_view(), name='search_results'),
   path('index/', views.index, name="index" ),
   path('index/<int:posts_id>/user/', views.user_index, name="user_index" ),
-  path('index/<int:posts_id>/user/add_profile_photo/', views.add_profile_photo, name='add_profile_photo'),
   path('index/<int:posts_id>/', views.posts_detail, name='detail'),
   path('index/create/', views.PostCreate.as_view(), name='posts_create'),
   path('index/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
